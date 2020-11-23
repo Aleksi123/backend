5513ae2c5a1cac5a7706cd9ef3e82e2ac4424331
Aleksi <a12345@live.fi>
2020-11-23 15:38:14 +0200

    Enabled cors for backend

--- src/main/java/com/example/backend/MainController.java
+++ src/main/java/com/example/backend/MainController.java
@@ -2,14 +2,12 @@ package com.example.backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping(path="/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MainController {
   @Autowired 
   private TodoRepository todoRepository;
